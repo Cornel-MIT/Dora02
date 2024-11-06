446a36b14f3ea47a773d6b877fdd140016231987
Cornel <www.ccornel@gmail.com>
2024-11-06 10:21:56 +0200

    Added Icons on the Nav-bar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import '../components/styles/Header.css';
+import { FaShoppingCart } from 'react-icons/fa';  
+import { FaBars, FaTimes } from 'react-icons/fa';
+import logo from '../components/media/Dora-Logo2.png';  
+import '../components/styles/Header.css';  
 
 const Header = () => {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+  const [cartCount, setCartCount] = useState(5);
+
+  const toggleMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
+
   return (
     <header className="header">
       <nav className="navbar">
-        <Link to="/" className="nav-link">Home</Link>
-        <Link to="/add-product" className="nav-link">Add Product</Link>
-        <Link to="/cart" className="nav-link">Cart</Link>
-        <Link to="/register" className="nav-link">Register</Link>
-        <Link to="/login" className="nav-link">Login</Link>
+        <div className="logo">
+          <Link to="/">
+            <img src={logo} alt="Logo" className="logo-img" />
+          </Link>
+        </div>
+        
+        <div className="hamburger-menu" onClick={toggleMenu}>
+          {isMobileMenuOpen ? <FaTimes size={30} /> : <FaBars size={30} />}
+        </div>
+
+        <div className={`nav-links ${isMobileMenuOpen ? 'active' : ''}`}>
+          <Link to="/" className="nav-link">Home</Link>
+          <Link to="/add-product" className="nav-link">Add Product</Link>
+          <Link to="/cart" className="nav-link cart-icon">
+            <FaShoppingCart size={24} />
+            {cartCount > 0 && <span className="cart-count">{cartCount}</span>}
+          </Link>
+          <Link to="/register" className="nav-link">Register</Link>
+          <Link to="/login" className="nav-link">Login</Link>
+        </div>
       </nav>
     </header>
   );
